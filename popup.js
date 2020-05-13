f8a4e303bc368a64c8d120eb4066f89c742d3b22
CodHeK <gaganganapathyas@yahoo.in>
2020-05-13 22:11:58 +0800

    done

---
+++ popup.js
@@ -0,0 +1,64 @@
+let scroll_debounce = null, popUp = false;
+
+const getDocumentHeight = () => {
+    let height = Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+    );
+
+    return height;
+}
+
+const buildPopUp = (windowScroll) => {    
+    const documentSize = getDocumentHeight();
+
+    let percentage = Math.round(((windowScroll + document.documentElement.clientHeight) * 100)/documentSize);
+
+    const top = (percentage * document.documentElement.clientHeight) / 100;
+
+    const $popUp = document.createElement('div');
+
+    $popUp.textContent = `${percentage}%`;
+    $popUp.id = 'scroller-popup';
+    
+    $popUp.style.cssText = `
+        position: absolute;
+        width: 80px;
+        height: 25px;
+        border-radius: 10px;
+        text-align: center;
+        padding-top: 5px;
+        font-size: 14px;
+        font-family: "Courier New", Courier, monospace;
+        letter-spacing: 1.5px;
+        z-index: 1000;
+        top: ${windowScroll + top - 30}px;
+        right: 5px;
+        background-color: transparent;
+        color: #000;
+        font-weight: bold;
+    `;
+
+    document.body.appendChild($popUp);
+    popUp = true;
+}
+
+const removePopUp = () => {
+    const $popUp = document.getElementById('scroller-popup');
+
+    document.body.removeChild($popUp);
+    popUp = false;
+}
+
+window.addEventListener('scroll', e => {
+    clearTimeout(scroll_debounce);
+
+    popUp && removePopUp();
+
+    scroll_debounce = setTimeout(() => {
+        const windowScroll = window.scrollY;
+
+        !popUp && buildPopUp(windowScroll);
+    }, 200);
+});
